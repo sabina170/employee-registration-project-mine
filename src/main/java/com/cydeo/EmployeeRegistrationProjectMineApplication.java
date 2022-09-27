631b39e140c276c1dc7c97538fce885c21835e83
Sabina Omarova <sabomar170@gmail.com>
2022-09-27 11:17:38 -0400

    Initial comment

---
+++ src/main/java/com/cydeo/EmployeeRegistrationProjectMineApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRegistrationProjectMineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeRegistrationProjectMineApplication.class, args);
+    }
+
+}
